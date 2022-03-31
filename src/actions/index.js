232407d96aea30dfb80522a01dfc0de9da817d0d
Felipe Rangel <feliperangel277@gmail.com>
2022-03-31 16:27:26 -0300

    Requisito 1 - Implementa Redux e cria uma pagina inicial de login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 // Coloque aqui suas actions
+export const CHANGE_EMAIL = 'CHANGE_EMAIL';
+
+export const CHANGE_WALLET = 'CHANGE_WALLET';
+
+export const changeEmail = (email) => ({
+  type: CHANGE_EMAIL,
+  email,
+});
+
+export const changeWallet = (state) => ({
+  type: CHANGE_WALLET,
+  state,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,81 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { changeEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+  }
+
+  changeForm = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, () => this.validateForm());
+  }
+
+  validateForm = () => {
+    const { email, password } = this.state;
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const passwordMinLength = 6;
+    if (emailRegex.test(email) && password.length >= passwordMinLength) {
+      this.setState({
+        isDisabled: false,
+      });
+    } if (!emailRegex.test(email) || password.length < passwordMinLength) {
+      this.setState({
+        isDisabled: true,
+      });
+    }
+  }
+
+  saveEmailAndExit = () => {
+    const { saveEmail, history } = this.props;
+    const { email } = this.state;
+    saveEmail(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled } = this.state;
+    return (
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          onChange={ this.changeForm }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          onChange={ this.changeForm }
+        />
+        <button
+          type="button"
+          disabled={ isDisabled }
+          onClick={ this.saveEmailAndExit }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  saveEmail: (email) => dispatch(changeEmail(email)),
+});
+
+Login.propTypes = {
+  saveEmail: propTypes.func,
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(Login);
